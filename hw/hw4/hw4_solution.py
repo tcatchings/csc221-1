38ae8ea7f725826664a7e7399a4d26039cc964ff
brandonrandle <brandonrandle@students.belhaven.edu>
2016-03-23 17:13:43 +0000

    functions to test added, except last one

--- hw/hw4/hw4_solution.py
+++ hw/hw4/hw4_solution.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python3
 
 def is_odd(number):
     return number%2==1
+
+def is_even(number):
+    return number%2==0
+
+def is_mult_of_four(number):
+    return number%4==0
+
+def is_mult_of_x(number, divisor):
+    return number % divisor == 0
+
+def both_ends(s):
+ 
